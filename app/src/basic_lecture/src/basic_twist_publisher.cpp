89525b5b7f1686b5b4d8cb0392866e2dbb93fbb0
Yuhei Nakayama (gon) <yuhei.nakayama@pixiedusttech.com>
2019-11-19 17:34:18 +0900

    tornade kame

--- app/src/basic_lecture/src/basic_twist_publisher.cpp
+++ app/src/basic_lecture/src/basic_twist_publisher.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <ctime> 
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "basic_twist_publisher");
@@ -9,13 +10,16 @@ int main(int argc, char** argv) {
   ros::Publisher cmd_pub = n.advertise<geometry_msgs::Twist>("cmd_vel", 1000);
   ros::Rate loop_rate(10);
 
+  std::time_t start = std::time(0);
+
   while (ros::ok()) {
+    long elapsed = std::time(0) - start;
     geometry_msgs::Twist cmd_vel;
     cmd_vel.linear.x = 1.0;
+    cmd_vel.angular.z = elapsed * 0.1;
     cmd_pub.publish(cmd_vel);
     ros::spinOnce();
     loop_rate.sleep();
-    ROS_INFO("published");
   }
   return 0;
 }
